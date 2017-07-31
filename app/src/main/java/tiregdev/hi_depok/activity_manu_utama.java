105b9e6cd40af86ca835004cdb0a8abd378bf739
JAR-FIS <fsandjujar@gmail.com>
2017-07-31 09:24:25 +0700

    first commit

---
+++ app/src/main/java/tiregdev/hi_depok/activity_manu_utama.java
@@ -0,0 +1,13 @@
+package tiregdev.hi_depok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_manu_utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu_utama);
+    }
+}
