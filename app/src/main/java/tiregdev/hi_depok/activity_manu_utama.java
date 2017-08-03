0f15718f844b03737b6c425e4aaca5075b6038ac
JAR-FIS <fsandjujar@gmail.com>
2017-08-03 10:30:15 +0700

    First commit, added welcome activity, splash screen, login, sign up, and forgot password

--- app/src/main/java/tiregdev/hi_depok/activity_manu_utama.java
+++ app/src/main/java/tiregdev/hi_depok/activity_manu_utama.java
@@ -1,13 +1,29 @@
 package tiregdev.hi_depok;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_manu_utama extends AppCompatActivity {
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
+public class activity_manu_utama extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manu_utama);
+        setContentView(R.layout.activity_menu_utama);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_news) {
+                    // The tab with id R.id.tab_favorites was reselected,
+                    // change your content accordingly.
+                }
+            }
+        });
     }
 }
